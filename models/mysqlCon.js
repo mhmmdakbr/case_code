a071dd5fe0d97ff998581c2385c134c00ffcfaeb
rizky triantoro <rizq.tri.antoro@gmail.com>
2019-07-11 07:12:44 +0000

    Update mysqlCon.js

--- models/mysqlCon.js
+++ models/mysqlCon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "127.0.0.1",
-  port: "8889",
-  user: "rizq",
-  password: "summer",
+  port: "3306",
+  user: "root",
+  password: "muslimpocket2019!@#",
   database: "poin"
 });
 
