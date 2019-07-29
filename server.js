ab478bb9eeb8b32306b2d5f6615695d583f996c3
Chalvin189 <chalvin.dinata@gmail.com>
2019-07-29 11:17:29 +0700

    server 3002

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ function main () {
     app.set('view engine', 'hbs');
     
     
-    app.listen(3001, () => {
-        console.log('Express server started at port : 3001');
+    app.listen(3002, () => {
+        console.log('Express server started at port : 3002');
     });
     
     app.use('/rekons', rekonsController);
